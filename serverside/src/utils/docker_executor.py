9621ee7f90032f3ee31e71b96395ca9742941e3f
ivan-afonichkin <ivan.afonichkin@gmail.com>
2024-05-04 20:51:26 +0100

    Updates

--- serverside/src/utils/docker_executor.py
+++ serverside/src/utils/docker_executor.py
@@ -1,6 +1,10 @@
 import base64
 from dataclasses import dataclass
+import json
+import logging
+import tempfile
 import time
+from uuid import uuid4
 from src.config import GITHUB_APP_ID, GITHUB_APP_PRIVATE_KEY_BASE64
 from src.utils.integrations.github_integration import RepoHelper
 import jwt
@@ -12,13 +16,14 @@ from pathlib import Path
 @dataclass
 class TestCoverageItem:
     coverage: float
-    missing_lines: list[tuple[int, int]]
+    missing_lines: list[int]
 
+
+logging.basicConfig(level=logging.INFO)
 
 class PytestOutput:
     def __init__(self, test_coverage: dict[Path, TestCoverageItem]):
         self.test_coverage = test_coverage
-
 
 class DockerExecutor:
     def __init__(self, repo_url):
@@ -27,17 +32,8 @@ class DockerExecutor:
         """
         self.repo_url = repo_url
         self.repo_helper = RepoHelper(repo_url=self.repo_url)
-        
-        APP_ID = GITHUB_APP_ID
-        PRIVATE_KEY = base64.b64decode(GITHUB_APP_PRIVATE_KEY_BASE64).decode("utf-8")
-        installation = self.repo_helper.get_installation_by_url(self.repo_url)
 
-        jwt_key = self.generate_jwt(APP_ID, PRIVATE_KEY)
-        access_token = self.get_installation_access_token(installation.id, jwt_key)
-
-        self.clone_repository(repo_url, access_token)
-
-    def generate_jwt(self, app_id, private_key):
+    def _generate_jwt(self, app_id, private_key):
         payload = {
             'iat': int(time.time()) - 60,  # Issued at time
             'exp': int(time.time()) + 600,  # JWT expiration time
@@ -46,23 +42,46 @@ class DockerExecutor:
         token = jwt.encode(payload, private_key, algorithm='RS256')
         return token
 
-    def get_installation_access_token(self, installation_id, jwt):
+    def _get_installation_access_token(self, installation_id, jwt):
         headers = {
             'Authorization': f'Bearer {jwt}',
             'Accept': 'application/vnd.github.v3+json'
         }
         url = f'https://api.github.com/app/installations/{installation_id}/access_tokens'
         response = requests.post(url, headers=headers)
-        print(response.json())
         return response.json()['token']
 
-    def clone_repository(self, repo_url, access_token):
+    def _clone_repository(self, repo_url: str, access_token: str, output_path: Path):
         # Modify the repo URL to include the access token
         auth_repo_url = repo_url.replace('https://', f'https://x-access-token:{access_token}@')
-        subprocess.run(['git', 'clone', auth_repo_url, 'test_repo'])
+        cmd = f'git clone {auth_repo_url} {output_path}'
+        logging.info(f'Running command: {cmd}')
+        subprocess.run(cmd.split(' '))
 
+    def _build_container(self, tag: str, repo_path: Path):
+        cmd = f'docker build -f {repo_path / "Dockerfile.cf"} -t {tag} {repo_path}'
+        logging.info(f'Running cmd: {cmd}')
+        subprocess.run(cmd.split(' '))
 
-    def execute_with_new_files(new_files: dict[str, str]) -> PytestOutput:
+    def _run_tests_and_get_coverage(self, tag: str) -> PytestOutput:
+        cmd = f'docker run -t {tag} /bin/bash -c "pytest --cov=. --cov-report json >/dev/null && cat coverage.json"'
+        logging.info(f'Running cmd: {cmd}')
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+        output = json.loads(proc.stdout.read().decode('utf-8'))
+        
+        test_coverage = {
+            key: TestCoverageItem(coverage=float(info_dict['summary']['percent_covered']), missing_lines=list(info_dict['missing_lines'])) for key, info_dict in output['files'].items()
+        }
+
+        return PytestOutput(test_coverage=test_coverage)
+
+    def _create_files(self, repo_dir: Path, new_files: dict[str, str]):
+        for file_path, contents in new_files:
+            with open(repo_dir / file_path, 'w') as f:
+                logging.info(f'Creating new file at: {repo_dir / file_path}')
+                f.write(contents)
+
+    def execute_with_new_files(self, new_files: dict[str, str]) -> PytestOutput:
         """
         new_files: 
             {
@@ -74,11 +93,29 @@ class DockerExecutor:
         run tests from command with coverage
         return PytestOutput
         """
-        pass
+        APP_ID = GITHUB_APP_ID
+        PRIVATE_KEY = base64.b64decode(GITHUB_APP_PRIVATE_KEY_BASE64).decode("utf-8")
+        installation = self.repo_helper.get_installation_by_url(self.repo_url)
+
+        jwt_key = self._generate_jwt(APP_ID, PRIVATE_KEY)
+        access_token = self._get_installation_access_token(installation.id, jwt_key)
+
+        with tempfile.TemporaryDirectory(dir=Path.cwd()) as repo_dir:
+            logging.info(f'Created temporary directory to clone repo: {repo_dir}')
+            self._clone_repository(self.repo_url, access_token, output_path=repo_dir)
+            tag = str(uuid4()).split('-')[0]
+            self._build_container(tag=tag, repo_path=Path(repo_dir))
+            if len(new_files) > 0:
+                self._create_files(Path(repo_dir), new_files)
+
+            pytest_output = self._run_tests_and_get_coverage(tag=tag)
+            return pytest_output
 
 
 def main():
-    DockerExecutor('https://github.com/CaptureFlow/captureflow-py')
+    docker_executor = DockerExecutor('https://github.com/CaptureFlow/captureflow-py')
+    pytest_output = docker_executor.execute_with_new_files({})
+    print(pytest_output.test_coverage)
 
 
 if __name__ == '__main__':
