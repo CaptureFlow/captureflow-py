547610d09c6b6726a02cd8e8687a82753fc3dc2a
ivan-afonichkin <ivan.afonichkin@gmail.com>
2024-05-06 22:45:43 +0100

    Updated config

--- serverside/src/config.py
+++ serverside/src/config.py
@@ -1,3 +1,4 @@
+import base64
 import os
 
 from dotenv import load_dotenv
@@ -5,6 +6,6 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 
 GITHUB_APP_ID = os.getenv("GITHUB_APP_ID", "EMPTY_GITHUB_APP_ID")
-GITHUB_APP_PRIVATE_KEY_BASE64 = os.getenv("GITHUB_APP_PRIVATE_KEY_BASE64", "EMPTY_GITHUB_APP_PRIVATE_KEY_BASE64")
+GITHUB_APP_PRIVATE_KEY_BASE64 = os.getenv("GITHUB_APP_PRIVATE_KEY_BASE64", base64.b64encode(b"EMPTY_GITHUB_APP_PRIVATE_KEY_BASE64"))
 OPENAI_KEY = os.getenv("OPENAI_KEY", "EMPTY_OPENAI_KEY")
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
