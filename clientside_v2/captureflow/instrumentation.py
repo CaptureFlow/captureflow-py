5275ee821ddf36e7e7b7d6923e13ec58e65e2c07
Nikita Kutz <chaozik1337@gmail.com>
2024-06-10 22:07:03 +0200

    Add instrumentation hooks for common packages

--- clientside_v2/captureflow/instrumentation.py
+++ clientside_v2/captureflow/instrumentation.py
@@ -55,6 +55,92 @@ def _instrument_requests(tracer_provider: TracerProvider):
     except ImportError as e:
         pass
 
+def _instrument_httpx(tracer_provider: TracerProvider):
+    try:
+        import httpx
+        from opentelemetry.instrumentation.httpx import HTTPXClientInstrumentor, RequestInfo, ResponseInfo
+
+        async def read_stream(stream):
+            if stream is None:
+                return None
+            content = b""
+            async for chunk in stream:
+                content += chunk
+            return content
+
+        async def request_hook(span, request: RequestInfo):
+            if span.is_recording():
+                span.set_attribute("http.request.method", request.method.decode())
+                span.set_attribute("http.request.url", str(request.url))
+                span.set_attribute("http.request.headers", str(request.headers))
+                body = await read_stream(request.stream)
+                if body:
+                    span.set_attribute("http.request.body", _decode_body(body))
+                    new_stream = httpx.ByteStream([body])  # Reset the stream for the actual request
+                    request = httpx.Request(
+                        method=request.method,
+                        url=request.url,
+                        headers=request.headers,
+                        stream=new_stream,
+                        extensions=request.extensions,
+                    )
+
+        async def response_hook(span, request: RequestInfo, response: ResponseInfo):
+            if span.is_recording():
+                span.set_attribute("http.response.status_code", response.status_code)
+                span.set_attribute("http.response.headers", str(response.headers))
+                body: httpx.AsyncResponseStream = response.stream # AsyncResponseStream
+                # pass ???
+ 
+        # Do we actually need sync hooks?
+        HTTPXClientInstrumentor().instrument(
+            tracer_provider=tracer_provider,
+            async_request_hook=request_hook,
+            async_response_hook=response_hook,
+        )
+    except ImportError as e:
+        pass
+
+def _instrument_flask(tracer_provider: TracerProvider):
+    try:
+        from opentelemetry.instrumentation.flask import FlaskInstrumentor
+        pass
+    except ImportError as e:
+        pass
+
+def _instrument_sqlalchemy(tracer_provider: TracerProvider):
+    try:
+        from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
+        pass
+    except ImportError as e:
+        pass
+
+def _instrument_sqlite3(tracer_provider: TracerProvider):
+    try:
+        from opentelemetry.instrumentation.sqlite3 import SQLite3Instrumentor
+        pass
+    except ImportError as e:
+        pass
+
+def _instrument_openai(tracer_provider: TracerProvider):
+    try:
+        from opentelemetry.instrumentation.openai import OpenAIInstrumentor
+        pass
+    except ImportError as e:
+        pass
+
 def apply_instrumentation(tracer_provider: TracerProvider):
+    # Web Frameworks
     _instrument_fastapi(tracer_provider)
+    _instrument_flask(tracer_provider)
+
+    # Generic HTTP request libraries
     _instrument_requests(tracer_provider)
+    _instrument_httpx(tracer_provider)
+
+    # Database interactions
+    _instrument_sqlalchemy(tracer_provider)
+    _instrument_sqlite3(tracer_provider)
+
+    # Other
+    _instrument_openai(tracer_provider)
--- clientside_v2/examples/server.py
+++ clientside_v2/examples/server.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import requests
+import httpx
 
 app = FastAPI()
 
@@ -7,10 +8,29 @@ def external_call():
     response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
     return response.json()
 
+async def external_call_httpx():
+    async with httpx.AsyncClient() as client:
+        response = await client.get("https://jsonplaceholder.typicode.com/posts/2")
+    return response.json()
+
+async def external_post_call_httpx():
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            "https://jsonplaceholder.typicode.com/posts",
+            json={"title": "foo", "body": "bar", "userId": 1}
+        )
+    return response.json()
+
 @app.get("/")
 async def read_root():
     data = external_call()
-    return {"message": "Hello World", "data": data}
+    data_post_httpx = await external_post_call_httpx()
+    data_get_httpx = await external_call_httpx()
+    return {
+        "message": "Hello World", 
+        "data": data,
+    }
+
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
